324bcbee79fafc04e1b7a547d825f8e603447f77
katojun <jkky0225@gmail.com>
2022-05-10 23:37:02 +0900

    add:jwt

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module junchi4250/jwt-handson
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+)
+
+require (
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"junchi4250/jwt-handson/auth"
 	"log"
 	"net/http"
 
@@ -18,6 +19,8 @@ func main() {
     r := mux.NewRouter()
     // localhost:8080/publicでpublicハンドラーを実行
 	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
     
     //サーバー起動
 	if err := http.ListenAndServe(":8080", r); err != nil {
@@ -30,6 +33,15 @@ var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
 		Tag:   "Vue.js",
 		URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
+		Tag:   "Go",
+		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
 	}
 	json.NewEncoder(w).Encode(post)
 })
